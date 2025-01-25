fd8fc683ea0d20cab1d9e31bbb03554b6f22a62a
Trevino Parker <ready4codingg@gmail.com>
2025-01-25 11:59:14 -0600

    Update phishingFakeSoftwareUpdate.ps1

--- phishingFakeSoftwareUpdate.ps1
+++ phishingFakeSoftwareUpdate.ps1
@@ -1,11 +1,26 @@
+# Define the log file path
+$logFile = "C:\ProgramData\entropygorilla.log"
+$scriptName = "FakeUpdatePopUp.ps1"
+
+# Function to log messages
+function Log-Message {
+    param (
+        [string]$message,
+        [string]$level = "INFO"
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "$timestamp [$level] [$scriptName] $message"
+    Add-Content -Path $logFile -Value $logEntry
+}
+
 # Function to show a fake software update alert
 function Show-FakeUpdatePopUp {
     Add-Type -AssemblyName PresentationFramework
     [System.Windows.MessageBox]::Show(
         "A critical software update is required for your system. Click 'OK' to download and install the update now.",
         "Software Update",
-        "OKCancel",
-        "Warning"
+        [System.Windows.MessageBoxButton]::OKCancel,
+        [System.Windows.MessageBoxImage]::Warning
     )
 }
 
@@ -14,3 +29,4 @@ Log-Message "Displaying simulated fake software update pop-up."
 
 # Show the fake update pop-up
 Show-FakeUpdatePopUp
+
